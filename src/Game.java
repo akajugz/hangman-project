4c63ff0713c78687e5f92b7cdf45048d4bce612b
Jugraj Singh <jsbains96@gmail.com>
2023-09-20 02:17:16 +0100

    Added the ASCII to the game.

--- src/Game.java
+++ src/Game.java
@@ -43,6 +43,89 @@ public class Game {
             lives--;
             System.out.println("Incorrect guess! Lives remaining: " + lives);
             System.out.println("Letters guessed: " + guessedLetters);
+
+            if (lives == 9) {
+                System.out.println("___/|____");
+            } else if (lives == 8) {
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 7) {
+                System.out.println("    _________");
+                System.out.println("    |/    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 6) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 5) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |     (   )");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 4) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |     (   )");
+                System.out.println("    |       |");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 3) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |     (   )");
+                System.out.println("    |       |");
+                System.out.println("    |       |");
+                System.out.println("    |    ");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 2) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |     (   )");
+                System.out.println("    |       |");
+                System.out.println("    |       |");
+                System.out.println("    |      / /");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 1) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |     (-_-)");
+                System.out.println("    |       |");
+                System.out.println("    |       |");
+                System.out.println("    |      / /");
+                System.out.println("    |    ");
+                System.out.println("___/|____");
+            } else if (lives == 0) {
+                System.out.println("    _________");
+                System.out.println("    |/      |");
+                System.out.println("    |       |");
+                System.out.println("    |     (-_-)");
+                System.out.println("    |       |");
+                System.out.println("    |       |");
+                System.out.println("    |      | |");
+                System.out.println("___/|____ GAME OVER");
+            }
         }
     }
 
